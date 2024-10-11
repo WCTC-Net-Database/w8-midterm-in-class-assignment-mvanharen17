c45ec2b99e4815f72a131b71e52b1422bbaf99fe
mvanharen17 <melanie.vanharen@gmail.com>
2024-10-10 22:45:14 -0500

    Add files via upload
    finished

--- Program.cs
+++ Program.cs
@@ -1,15 +1,37 @@
-﻿using System;
-using System.IO;
-using System.Reflection.Emit;
-using System.Xml.Linq;
+﻿using Microsoft.Extensions.DependencyInjection;
+using W8_assignment_template.Data;
+using W8_assignment_template.Helpers;
+using W8_assignment_template.Interfaces;
+using W8_assignment_template.Models.Characters;
+using W8_assignment_template.Models.Rooms;
+using W8_assignment_template.Services;
 
-namespace CharacterConsole;
+namespace W8_assignment_template;
 
-class Program
+internal class Program
 {
-    static void Main()
+    private static void ConfigureServices(IServiceCollection services)
     {
-        var manager = new CharacterManager();
-        manager.Run();
+        // Register for DI
+        services.AddTransient<GameEngine>();
+        services.AddTransient<MenuManager>();
+        services.AddTransient<MapManager>();
+        services.AddSingleton<OutputManager>();
+
+        services.AddTransient<IRoom, Room>();
+        services.AddTransient<IRoomFactory, RoomFactory>();
+        services.AddSingleton<IContext, DataContext>();
+        
+    }
+
+    private static void Main(string[] args)
+    {
+        var serviceCollection = new ServiceCollection();
+        ConfigureServices(serviceCollection);
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+
+        var gameEngine = serviceProvider.GetService<GameEngine>();
+        gameEngine?.Run();
     }
 }
